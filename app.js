48fd17803db51522ee7bcbccac85de57961f2cd4
SevenSecrets <freddieshughes@googlemail.com>
2020-08-05 22:30:00 +0100

    rebuilding package.jsons and making frontend run via express server

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const path = require("path");
+const port = process.env.PORT || 3001;
 const db = require('./db');
 const Project = require('./models/project-model');
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+app.use(express.static(path.resolve(__dirname, './portfolio-frontend/build')));
+
 app.get('/', (req, res) => {
     res.send("Hello!")
-});
+})
 
 app.get("/projects", (req, res) => {
     var projects = Project.find({}, (err, response) => {
@@ -16,21 +19,26 @@ app.get("/projects", (req, res) => {
     })
 })
 
-app.post('/projects', (req, res) => {
-    res.send('this is where new project data will be sent.')
-});
 
-app.get("/projects/:projectId", (req, res) => {
-    res.send("this is where you'll get individual projects by id.")
-})
+// CURRENTLY UNNEEDED BUT I WANTED TO MAKE THEM ANYWAY
+//
+// app.post('/projects', (req, res) => {
+//     res.send('this is where new project data will be sent.')
+// });
 
-app.put('/projects/:projectId', (req, res) => {
-    res.send("this is where you'll update a project according to id.")
-});
+// app.get("/projects/:projectId", (req, res) => {
+//     res.send("this is where you'll get individual projects by id.")
+// })
 
-app.delete("/projects/:projectId", (req, res) => {
-    res.send("this is where you'll delete a project according to id.")
-})
+// app.put('/projects/:projectId', (req, res) => {
+//     res.send("this is where you'll update a project according to id.")
+// });
+
+// app.delete("/projects/:projectId", (req, res) => {
+//     res.send("this is where you'll delete a project according to id.")
+// })
+
+
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}.`)
