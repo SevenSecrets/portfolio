f625af22543340e77409414dd877b24f258f8c4e
SevenSecrets <freddieshughes@googlemail.com>
2020-08-10 13:26:15 +0100

    reverting to no https to see if it fixes mobile issues

--- app.js
+++ app.js
@@ -9,17 +9,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(express.static(path.resolve(__dirname, './portfolio-frontend/build')));
 
-if (process.env.NODE_ENV === "production") {
-    app.use((req, res, next) => {
-        if (req.header('x-forwarded-proto') !== 'https') {
-            console.log("redirected to https")
-            res.redirect(`https://${req.header('host')}${req.url}`)
-        } else {
-            next()
-        }
-    })
-}
-
 app.get('/', (req, res) => {
     res.send("Hello!")
 })
